22f73907af03a8caf5d152fb55d231f81ea53303
Ariel Llanita <llanitaariel@gmail.com>
2021-10-25 21:49:48 +0800

    Add useDarkMode hook

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+
+const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.log(error);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value) => {
+    try {
+      const valueToStore = value instanceof Function ? value(storedValue) : value;
+      setStoredValue(valueToStore);
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return [storedValue, setValue];
+};
+
+/**
+ * Custom hook for toggling the app's theme. The user's preference will then
+ * be stored to localStorage
+ */
+export default function useDarkMode() {
+  const [enabled, setEnabled] = useLocalStorage('dark-theme');
+  const isEnabled = typeof enabledState === 'undefined' && enabled;
+
+  useEffect(() => { 
+    const className = 'dark';
+    const bodyClass = window.document.body.classList;
+
+    isEnabled ? bodyClass.add(className) : bodyClass.remove(className);
+  }, [enabled, isEnabled]);
+
+  return [enabled, setEnabled];
+};
