272277a11f6cd60cb9c3ab8639bdcc3c0b6813a3
Ariel Llanita <llanitaariel@gmail.com>
2021-10-27 12:56:27 +0800

    Add route paths as contants variables

---
+++ src/constants/routes.js
@@ -0,0 +1,27 @@
+/* Public endpoints */
+export const LANDING_PAGE = '/';
+export const LOGIN = '/login';
+export const SIGN_UP = '/sign-up';
+export const SIGN_UP_VERIFY = "/sign-up/:url";
+export const RESET = '/reset';
+export const RESET_VERIFY = "/reset/:url";
+export const PUBLIC_ENDPOINT = [
+  LANDING_PAGE,
+  LOGIN,
+  SIGN_UP,
+  SIGN_UP_VERIFY,
+  RESET,
+  RESET_VERIFY
+];
+
+/* Private endpoints */
+export const DASHBOARD = "/dashboard";
+export const DASHBOARD_PAYMENTS = "/dashboard/payments";
+export const DASHBOARD_REPORTS = "/dashboard/reports";
+export const DASHBOARD_FAQ = "/dashboard/faq";
+export const PRIVATE_ENDPOINT = [
+  DASHBOARD,
+  DASHBOARD_PAYMENTS,
+  DASHBOARD_REPORTS,
+  DASHBOARD_FAQ
+];
