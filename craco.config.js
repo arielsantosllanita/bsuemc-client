26d59f3a3d5aceb0998df44e85aa3cf152f116a7
Ariel Llanita <llanitaariel@gmail.com>
2021-10-28 15:32:15 +0800

    Utilize craco-alias dependency for absolute imports (import aliases)

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,15 @@
+const cracoAlias = require('craco-alias');
+
 module.exports = {
+  plugins: [
+    {
+      plugin: cracoAlias,
+      options: {
+        source: "jsconfig",
+        baseUrl: "./src"
+      }
+    }
+  ],
   style: {
     postcss: {
       plugins: [
