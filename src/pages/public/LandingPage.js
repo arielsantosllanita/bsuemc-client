618bb44c32f2ec3e885cfb913972973442efa502
Ariel Llanita <llanitaariel@gmail.com>
2021-10-30 10:25:15 +0800

    Route landing page to public pages

---
+++ src/pages/public/LandingPage.js
@@ -0,0 +1,11 @@
+import Index from '@components/LandingPage';
+import TopNav from '@components/TopNav';
+
+export default function LandingPage() {
+  return (
+    <>
+      <TopNav />
+      <Index />
+    </>
+  )
+}
