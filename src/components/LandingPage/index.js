7c9faebf11c077bd6c7ccf089b0638071b4a7e0a
Ariel Llanita <llanitaariel@gmail.com>
2021-10-30 09:57:03 +0800

    Add index file for all components of landing page

---
+++ src/components/LandingPage/index.js
@@ -0,0 +1,22 @@
+import Content from "./Content";
+import Header from "./Header";
+import Footer from './Footer';
+import Services from './Services';
+import FadeInWhenVisible from "helpers/FadeInWhenVisible";
+
+export default function Index() {
+  return (
+    <>
+      <Header />
+      <FadeInWhenVisible>
+        <Content />
+      </FadeInWhenVisible>
+      <FadeInWhenVisible>
+        <Services />
+      </FadeInWhenVisible>
+      <FadeInWhenVisible>
+        <Footer />
+      </FadeInWhenVisible>
+    </>
+  )
+}
