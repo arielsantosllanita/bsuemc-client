507bccb142fb3ed61f9aab76acce6a72e4aae238
Ariel Llanita <llanitaariel@gmail.com>
2021-11-04 21:41:08 +0800

    Discard lazy loading for private routes

--- src/routes/Private.js
+++ src/routes/Private.js
@@ -1,16 +1,30 @@
-import Spinner from '@components/Spinner';
-import React, { lazy, Suspense } from 'react';
+import Dashboard from '@components/Dashboard';
+import Home from '@private/Home';
+import Faqs from '@private/Faqs';
+import Payments from '@private/Payments';
+import Reports from '@private/Reports';
 import { Switch, Route } from 'react-router-dom';
-import * as ROUTES from '../constants/routes';
+import * as ROUTES from '@constants/routes';
+// import Spinner from '@components/Spinner';
+// import React, { lazy, Suspense } from 'react';
+// import ProtectedRoute from "../helpers/ProtectedRoute";
 
 export default function Private() {
-  const Dashboard = lazy(() => import('../components/Dashboard'));
-  
+  // const Home = lazy(() => import("@private/Home"));
+  // const FAQs = lazy(() => import('@private/Faqs'));
+  // const Payments = lazy(() => import('@private/Payments'));
+  // const Reports = lazy(() => import('@private/Reports'));
+
   return (
-    <Suspense fallback={<Spinner />}>
+    // <Suspense fallback={<Spinner />}>
+    <Dashboard>
       <Switch>
-        <Route path={ROUTES.DASHBOARD} component={Dashboard} />
+        <Route path={ROUTES.DASHBOARD} exact component={Home} />
+        <Route path={ROUTES.DASHBOARD_FAQ} component={Faqs} />
+        <Route path={ROUTES.DASHBOARD_PAYMENTS} component={Payments} />
+        <Route path={ROUTES.DASHBOARD_REPORTS} component={Reports} />
       </Switch>
-    </Suspense>
+    </Dashboard>
+    // </Suspense>
   )
 }
