2cf2e85982775717c68430a5b7534b9da88610f0
Ariel Llanita <llanitaariel@gmail.com>
2021-11-28 14:23:53 +0800

    Use grid to layout dashboard contents

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -7,19 +7,21 @@ export default function Index({ children }) {
 
   return (
     <div className="h-screen overflow-y-auto">
-      <div className="flex">
-
+      <div className="grid grid-cols-1 lg:grid-cols-5">
+        {/* Sidenav */}
         <aside
           ref={sideNavRef}
-          className={`w-64 overflow-y-auto bg-gray-100 dark:bg-gray-600
-           fixed lg:sticky left-0 top-14 lg:top-0 lg:h-screen z-50 ${!openMenu ? "hidden lg:block" : ""}`}
+          className={"overflow-y-auto bg-gray-100 dark:bg-gray-600 "
+           + "w-64 lg:w-full fixed lg:sticky left-0 top-14 lg:top-0 lg:h-screen z-50 "
+           + "transform duration-700 ease "
+           + `${!openMenu ? "hidden lg:block" : ""}`}
           // ! The height of SideNav should be deducted by the height of the Header component (h-14 == 3.5rem)
           style={!openMenu ? {} : { height: 'calc(100% - 3.5rem)' }}
         >
           <SideNav />
         </aside>
 
-        <main className="flex-grow">
+        <main className="col-span-full lg:col-span-4">
           <Header openMenu={openMenu} setOpenMenu={setOpenMenu} />
           <div className="px-8 py-9 lg:px-7">
             {children}
